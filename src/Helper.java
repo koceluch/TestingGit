7e84137672210e487507e50f7354c4d8e162c8bb
Lukasz Grigoryev <koceluch@hm.edu>
2018-02-09 12:40:28 +0100

    Revert "Added a bug because i am very nasty."
    This reverts commit 4ff27f33f5cb796cf01ab6dba3690d839f13b7b4.

--- src/Helper.java
+++ src/Helper.java
@@ -2,7 +2,7 @@
 public class Helper {
 	public static void print(String... messages) {
 		for(String msg : messages) {
-			System.out.println(msg);21342412412
+			System.out.println(msg);
 		}
 	}
 }
